c069c58eb023326ad9a020957ca69e88fe181537
johanncolenetz <62523278+johanncolenetz@users.noreply.github.com>
2021-07-20 16:50:24 -0300

    Mudanças JS

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,34 @@
-alert ("hello World")
+var nome = "Johann Colenetz";
+var cargo = "Diretor de Fotografia - Stereogram";
+
+var nomeHtml = document.getElementById("nome-no-html");
+var cargohtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarNomeNoHtml(nome){
+    nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo){
+    cargohtml.innerHTML = cargo;
+}
+
+function logarNome(){
+    console.log(nome);
+}
+
+function cliqueNoBotaoProjetos(){
+    console.log("Clicou no Botão Projetos");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+
+function cliqueNoSobre(){
+    console.log("Clicou no botão Sobre");
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+}
+
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);
